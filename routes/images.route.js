c737d18d75188f35018cf4c588cdb5da1365e871
andreas1565 <42369642+andreas1565@users.noreply.github.com>
2019-08-26 07:29:05 +0200

    add images to the dashbord menu and
    and  middelware to images

--- routes/images.route.js
+++ routes/images.route.js
@@ -1,27 +1,30 @@
 const {getimages, showimagesform,  editimages,  deleteimages} = require('../controllers/images.controller');
+const isauthorized = require('../midleware/isauthorized');
+const isemployee = require('../midleware/isemployee');
+const isadmin = require('../midleware/isadmin');
 module.exports = function(app){
     /**
     * denne fuktion håndter get metoden for endpointet /images
      * @param {Function} app  express objektet
      */
-    app.get('/images', getimages);
+    app.get('/images', isauthorized, isemployee,getimages);
 
      /**
      * denne fuktion håndter get metoden for endpointet /editimage/:id
      * @param {Function} app  express objektet
      */
 
-    app.get('/editimage/:id', showimagesform);
+    app.get('/editimage/:id', isauthorized, isadmin, showimagesform);
 
     /**
      * denne funktion håndterer post metoden for endpointet /editimage/:id
      * @param {Function} app express objektet
      */
 
-    app.post('/editimage/:id', editimages);
+    app.post('/editimage/:id',isauthorized, isadmin, editimages);
     /**
      * denne fuktion håndter get metoden for endpointet /deleteimage/:id
      * @param {Function} app  express objektet
      */
-    app.get('/deleteimage/:id', deleteimages);
+    app.get('/deleteimage/:id', isauthorized, isadmin, deleteimages);
 }
