bd78d94fbb692376e2bf5e49efefd891a6d6ce6a
andreas1565 <42369642+andreas1565@users.noreply.github.com>
2019-08-28 11:39:23 +0200

    add comments and to  allproduct pagination

--- config/locals.js
+++ config/locals.js
@@ -1,11 +1,14 @@
 module.exports = function(app){
     app.use(function(req,  res, next){
+        // typeof er du tjæker hvikelen type noget er
         if(typeof  req.app.locals.isloggedin === 'undefined'){
             req.app.locals.isloggedin = false;
         }
+        // typeof er du tjæker hvikelen type noget er
         if(typeof  req.app.locals.userId === 'undefined'){
             req.app.locals.userId = false;
         }
+        // typeof er du tjæker hvikelen type noget er
         if(typeof req.app.locals.userlevel === "undefined"){
             req.app.locals.userlevel = false;
         }
